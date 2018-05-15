338220a1f67f7146a456459ae70a0995ea52648c
Adam Buglass <Adam Buglass@purple.local>
2018-05-15 15:49:07 +0100

    initial check-in

---
+++ grades.js
@@ -0,0 +1,45 @@
+var gradeBook = {
+	_grades: [],
+	
+	addGrade: function(grade){
+		this._grades.push(grade);
+	},
+	
+	getCountOfGrades: function(){
+		return this._grades.length;
+	},
+	
+	getTotalGradeScore: function(){
+		var totalGrades = 0;
+		for (var i = 0; i < this._grades.length; i++) {
+			totalGrades += this._grades[i];
+		}
+		return totalGrades;
+	},
+	
+	getAverageGradeScore: function(){
+		var gradeCounts = this.getCountOfGrades();
+		var totalGrades = this.getTotalGradeScore();
+		return totalGrades / gradeCounts;			
+	},
+	
+	getLetterGrade: function(){
+		var averageGradeScore = this.getAverageGradeScore();
+		if (averageGradeScore >= 90) // no switch?
+			return 'A';
+		else if (averageGradeScore >= 80)
+			return 'B';
+		else if (averageGradeScore >= 70)
+			return 'C';
+		else if (averageGradeScore >= 60)
+			return 'D';
+		else
+			return 'F';
+	},
+	
+	clear: function(){
+		this._grades = [];
+	}
+};
+
+exports.book = gradeBook;
---
+++ tests/gradesTests.js
@@ -0,0 +1,135 @@
+var testBook = require("../grades").book;
+
+exports["setUp"]=function(callBack) {
+	// Don't like this - I've just added a clear function purely for the tests :-(
+	// Can't I just create a new book?
+	testBook.clear();
+	callBack();
+};
+
+exports["Can add new grade"] = function(test) {
+	var expected = 1;
+	
+	testBook.addGrade(90);
+	var actual = testBook.getCountOfGrades();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+};
+
+exports["Can score total grades"] = function(test) {
+	var expected = 150;
+	
+	testBook.addGrade(100);
+	testBook.addGrade(50);
+	
+	var actual = testBook.getTotalGradeScore();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+};
+
+exports["Can average grades"] = function(test) {
+	var expected = 75;
+	
+	testBook.addGrade(100);
+	testBook.addGrade(50);
+	
+	var actual = testBook.getAverageGradeScore();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+};
+
+exports["Can compute letter grade of A"] = function(test) {
+	var expected = 'A';
+	
+	testBook.addGrade(100);
+	testBook.addGrade(90);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
+
+exports["Can compute letter grade of B"] = function(test) {
+	var expected = 'B';
+	
+	testBook.addGrade(86);
+	testBook.addGrade(90);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
+
+exports["Can compute letter grade of C"] = function(test) {
+	var expected = 'C';
+	
+	testBook.addGrade(86);
+	testBook.addGrade(72);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
+
+exports["Can compute letter grade of borderline C"] = function(test) {
+	var expected = 'C';
+	
+	testBook.addGrade(70);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
+
+exports["Can compute letter grade of D"] = function(test) {
+	var expected = 'D';
+	
+	testBook.addGrade(66);
+	testBook.addGrade(59);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
+
+exports["Can compute letter grade of borderline D"] = function(test) {
+	var expected = 'D';
+	
+	testBook.addGrade(60);
+	testBook.addGrade(60);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
+
+exports["Can compute letter grade of F"] = function(test) {
+	var expected = 'F';
+	
+	testBook.addGrade(61);
+	testBook.addGrade(45);
+	
+	var actual = testBook.getLetterGrade();
+	
+	test.equal(expected, actual);
+	
+	test.done();
+}
